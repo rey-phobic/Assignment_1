90d26cbefb1d63cd574f895e64c6c94e54282cb6
_rey._s <67858564+rey-phobic@users.noreply.github.com>
2022-08-20 19:41:31 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,36 @@
+#ASSIGNMENT 1
+
+
+py or python --version
+
+#print script to Hello Python on command line
+print("Hello Python")
+
+#print Hello python using visual studio
+print("Hello Python")
+
+#print Hello on first line and Python on second line
+print("Hello \nPython")
+
+#print MySirG on screen
+print("MySirG")
+
+#print Teacher's Day on screen
+print("Teacher's Day") 
+
+#print the value of a variable.Variable contains your name.
+a= "Sreyas"
+print(a)
+
+#create variables to store name,age,qualification,years of expierence.
+a="Sreyas" 
+b=22
+c="MCA Final Year"
+d= 2
+print("Name:"+ a +" " + "Age:"+  str(b) +" " + "Qualification:"+ c +" " +"Years of Experience:"+ str(d));
+
+#Print  Integer Values of three variables each in a new line.
+a=2
+b=3
+c=6
+print(a,b,c, sep='\n')
